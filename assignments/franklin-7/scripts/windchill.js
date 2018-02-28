7ed4ac0e52425db271607b61861e5d8c414d4302
Alyssa Hundley <ahundley11@gmail.com>
2018-02-27 21:28:00 -0700

    2/27 JS

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -1,3 +1,18 @@
 
 
-var f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16 
+var s = document.getElementById("windSpeed");
+var h = document.getElementById("high");
+var l = document.getElementById("low");
+
+var t = (h+l)/2
+
+
+function windChill(s, t) {
+    
+    return 35.74 + (0.6215*t) - (35.75*(Math.pow(s,0.16)) + (0.4275*t)*(Math.pow(s,0.16));
+}
+    
+   document.getElementById("windChill").innerHTML = windChill;                              
+                                 
+
+                                  
