66a323d5afceb851d0881def07da592d751d52de
Alyssa Hundley <ahundley11@gmail.com>
2018-03-01 09:44:16 -0700

    Gallery-7 Picture Sizing

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -8,4 +8,4 @@ var l = parseInt(document.getElementById("low").innerHTML);
 
 var t = (h+l)/2;
 
-document.getElementById("windChill").innerHTML = 35.74 + (0.6215*t) - (35.75*(Math.pow(s,0.16))) + ((0.4275*t)*(Math.pow(s,0.16)));
+document.getElementById("windChill").innerHTML = (35.74 + (0.6215*t) - (35.75*(Math.pow(s,0.16))) + ((0.4275*t)*(Math.pow(s,0.16)))).toFixed(2);
