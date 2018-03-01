69ed88247cac7fb169ce9455a15c57880374555c
Alyssa Hundley <ahundley11@gmail.com>
2018-03-01 09:30:09 -0700

    WindChill Connection

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -1,18 +1,11 @@
+//JavaScript (put at the bottom near the </body>)
+
+           
+var s = parseInt(document.getElementById("windSpeed").innerHTML);
+var h = parseInt(document.getElementById("high").innerHTML);
+var l = parseInt(document.getElementById("low").innerHTML);
 
 
-var s = document.getElementById("windSpeed");
-var h = document.getElementById("high");
-var l = document.getElementById("low");
+var t = (h+l)/2;
 
-var t = (h+l)/2
-
-
-function windChill(s, t) {
-    
-    return 35.74 + (0.6215*t) - (35.75*(Math.pow(s,0.16)) + (0.4275*t)*(Math.pow(s,0.16));
-}
-    
-   document.getElementById("windChill").innerHTML = windChill;                              
-                                 
-
-                                  
+document.getElementById("windChill").innerHTML = 35.74 + (0.6215*t) - (35.75*(Math.pow(s,0.16))) + ((0.4275*t)*(Math.pow(s,0.16)));
